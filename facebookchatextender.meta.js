1a0baa1f9e1244a188881853de175ae422be5fc1
Hydra <iHydra@users.noreply.github.com>
2015-05-04 01:01:25 -0500

    Create facebookchatextender.meta.js

---
+++ facebookchatextender.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name          Facebook Chat Extender
+// @namespace     https://github.com/iHydra
+// @version       0.0.1
+// @description   Extends Facebook Chat's height and width for large screens.
+// @updateURL     https://github.com/iHydra/facebookchatextender/raw/master/facebookchatextender.meta.js
+// @downloadURL   https://github.com/iHydra/facebookchatextender/raw/master/facebookchatextender.user.js
+// @include       https://facebook.com/*
+// @include       https://www.facebook.com/*
+// @include       http://facebook.com/*
+// @include       http://www.facebook.com/*
+// @author        iHydra
+// @resource      CustomCSS https://github.com/iHydra/facebookchatextender/raw/master/stylesheet_0.0.1.css
+// @grant         GM_addStyle
+// @grant         GM_getResourceText
+// @run-at        document-start
+// ==/UserScript==
